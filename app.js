c9541734fed605381a8274269ae6a99358b816e2
Dwayne <saulsdwayne@gmail.com>
2016-10-11 11:47:12 +0200

    Finished the traffic rules

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
 //find the body tag and store it in a variable called 'body'
-var body = document.querySelector("body");
+  var body = document.querySelector("body");
 
-var taxiLocationCounter = 1;
+  var taxiLocationCounter = 1;
 
-body.onkeydown = function(e){
+  body.onkeydown = function(e){
+    var trafficLight = new TrafficLight(taxiLocationCounter);
 
     //right arrow : 39
     //left arrow  : 37
@@ -12,9 +13,54 @@ body.onkeydown = function(e){
     //down arrow  : 40
 
     displayMessage(e.keyCode);
+    if ((e.keyCode === 39) && (trafficLight.state() === 'green')) {
+      var previousLocationCounter = taxiLocationCounter;
+      taxiLocationCounter ++;
+
+    }
+    else if (e.keyCode === 37) {
+    var  previousLocationCounter = taxiLocationCounter;
+    taxiLocationCounter --;
+    }
+    else if (e.keyCode === 38 ) {
+
+    trafficLight.red();
+  }
+  else if (e.keyCode === 40) {
+  trafficLight.green();
+
+}
+  else if (true) {
+      trafficLight.orange();
+
+  }
+      moveTaxi(previousLocationCounter, taxiLocationCounter);
+
+
+    // //change the color like this
+    // trafficLight.green();
+    // trafficLight.red();
+    // trafficLight.orange();
+    //
+    // //get the current color of the traffic light
+    // trafficLight.state(taxiLocationCounter); // returns 'red', 'green' or 'orange'
+
+
+
+      //up arrow    : 38
+      //down arrow  : 40
+
+
+    /*if (moveTaxi == 38) {
+      var previousLocationCounter = taxiLocationCounter;
+      taxiLocationCounter --;
+    }
+    else if (moveTaxi == 40) {
+      var previousLocationCounter = taxiLocationCounter;
+      taxiLocationCounter ++;
+    }
 
     /*
-
     to move the taxi forward...
     when the right arrow is pressed
     work with `taxiLocationCounter`
@@ -26,9 +72,9 @@ body.onkeydown = function(e){
         taxiLocationCounter)` function
 
     */
-
     // to make the second TrafficLight `Orange`
-    var tl = new TrafficLight(2);
-    tl.orange();
+
+    // var tl = new TrafficLight(2);
+    // tl.orange();
 
 };
